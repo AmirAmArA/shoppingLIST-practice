205540c1dce89275338ccf6ded1c3eda7aa0c3f7
amir <amir@rowmy.org>
2020-12-16 18:09:34 +0300

    intial push

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import {v4 as uuid} from 'uuid';
+
+
+export class ShoppingList extends Component {
+  state = {
+    items: [
+      { id: uuid(), name: 'Eggs' },
+      { id: uuid(), name: 'Milk' },
+      { id: uuid(), name: 'Steak' },
+      { id: uuid(), name: 'Water' }
+    ]
+  }
+
+
+  render() {
+    const { items } = this.state;
+    return (
+      <Container>
+        <Button
+          color="dark"
+          style={{ marginBottom: '2rem' }}
+          onClick={() => {
+            const name = prompt('Enter Item');
+            if (name) {
+              this.setState(state => ({
+                items: [...state.items, { id: uuid(), name }]
+              }));
+            }
+          }}
+        >Add Item</Button>
+      </Container>
+    )
+  }
+}
+
+export default ShoppingList;
