41427c911954247f4121a1ad7c207c12623f2a1e
amir <amiramara11@gmail.com>
2020-12-17 10:53:28 +0200

    continue-on-proj

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import {v4 as uuid} from 'uuid';
-
+import React, { Component } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { v4 as uuid } from "uuid";
 
 export class ShoppingList extends Component {
   state = {
     items: [
-      { id: uuid(), name: 'Eggs' },
-      { id: uuid(), name: 'Milk' },
-      { id: uuid(), name: 'Steak' },
-      { id: uuid(), name: 'Water' }
-    ]
-  }
-
+      { id: uuid(), name: "Eggs" },
+      { id: uuid(), name: "Milk" },
+      { id: uuid(), name: "Steak" },
+      { id: uuid(), name: "Water" },
+    ],
+  };
 
   render() {
     const { items } = this.state;
@@ -21,18 +19,43 @@ export class ShoppingList extends Component {
       <Container>
         <Button
           color="dark"
-          style={{ marginBottom: '2rem' }}
+          style={{ marginBottom: "2rem" }}
           onClick={() => {
-            const name = prompt('Enter Item');
+            const name = prompt("Enter Item");
             if (name) {
-              this.setState(state => ({
-                items: [...state.items, { id: uuid(), name }]
+              this.setState((state) => ({
+                items: [...state.items, { id: uuid(), name }],
               }));
             }
           }}
-        >Add Item</Button>
+        >
+          Add ITEMS
+        </Button>
+        <ListGroup>
+          <TransitionGroup className="shopping-list">
+            {items.map(({ id, name }) => (
+              <CSSTransition key={id} timeout={500} className="shade">
+                <ListGroupItem>
+                  <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={() => {
+                      this.setState((state) => ({
+                        items: state.items.filter((item) => item.id !== id),
+                      }));
+                    }}
+                  >
+                    &times;
+                  </Button>
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
       </Container>
-    )
+    );
   }
 }
 
